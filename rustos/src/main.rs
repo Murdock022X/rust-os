6544368f03d58406a2c2c47a6e5fae0878a248fe
Henry Murdock <murdoc44@msu.edu>
2025-12-16 11:29:07 -0500

    Created bootimage

--- rustos/src/main.rs
+++ rustos/src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
 #[panic_handler]
@@ -10,6 +12,8 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    vga_buffer::print_something();
+    
     loop {}
 }
 
